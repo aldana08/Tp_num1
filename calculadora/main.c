b39206df1de8df2616e5796e9630daa670eca035
aldana08 <aldy.pereyra.desa@gmail.com>
2019-04-05 13:06:58 -0300

    cambios finales

--- calculadora/main.c
+++ calculadora/main.c
@@ -10,7 +10,8 @@ int main()
     char salir = 'n';
     int opcion;
 
-    do{
+    do
+        {
 
         printf("\n *------Menu de opciones------* \n\n");
         printf("Ingresar 1er operando (A = x).\n");
@@ -23,14 +24,17 @@ int main()
         printf("\n\n=============================\n\n");
 
 
-        do{
+        do
+            {
 
-            if(A==0 && B==0){
+                if(A==0 && B==0){
 
-                printf("Error. No se puede realizar operaciones con 0");
-                salir = 's';
+                    printf("Error. No se puede realizar operaciones con 0");
+                    salir = 's';
 
-            }else{
+            }else
+
+            {
 
                 printf("*------Seleccione la operacion a realizar------*.\n");
                 printf("\n\n=============================\n\n");
@@ -38,7 +42,7 @@ int main()
                 printf("2- Calcular Resta (%d - %d)\n",A,B,"\n");
                 printf("3- Calcular Division (%d / %d)\n",A,B,"\n");
                 printf("4- Calcular Multiplicacion (%d * %d)\n",A,B,"\n");
-                printf("5- Calcular Factorial de (!%d) y (!%d)\n",A,B);
+                printf("5- Calcular Factorial de (%d) y (%d)\n",A,B);
                 printf("6- Salir\n");
                 printf("\n\n=============================\n\n");
                 printf("\nIngrese opcion: ");
@@ -64,7 +68,7 @@ int main()
                         printf("\n\n=============================\n\n");
                         break;
                     case 5:
-                        printf("\ne- El factorial de !%d es: %d y el factorial de !%d es: %d\n",A, factorial(A), B, factorial(B));
+                        printf("\ne- El factorial de %d es: %d y el factorial de %d es: %d\n",A, factorial(A), B, factorial(B));
                         printf("\n\n=============================\n\n");
                         break;
                     case 6:
@@ -72,13 +76,23 @@ int main()
                         scanf(" %c",&salir);
                         printf("\nOpcion seleccionada: %c", salir);
                         printf("\n\n=============================\n\n");
-                        //system("pause");
                         break;
-                }
-            //system("pause");
-            }//fin del else
+
+                    default:
+
+                        printf("\n\nError. No ingreso una opcion valida. Seleccionar una opcion entre 1 y 6\n\n");
+                    }//fin del switch
+
+                }//fin del else
+
+
 
         }while(salir == 'n');
+
+        if(A==0 && B==0)
+        {
+            salir = 'n';
+        }
 
     }while(salir == 'n');
 
@@ -86,140 +100,4 @@ int main()
 
 }
 
-/*
 
-int main()
-{
-    char salir = 'n';
-    int i = 0;
-    int flag1 = 0;
-    int flag2 = 0;
-    int flag3 = 0;
-    int num1;
-    int num2;
-    int opcion;
-
-    do
-    {
-
-     if(flag1 == 0 && flag2 == 0)
-   {
-    printf("\n *------Menu de opciones------* \n\n");
-    printf("1- Ingresar 1er operando (A = x).\n");
-    printf("2- Ingresar 2do operando (B = y).\n");
-    printf("3- Calcular todas las operaciones.\n");
-    printf("4- Informar resultados.\n");
-    printf("5- Salir.\n");
-    printf("\n\n=============================\n\n");
-    printf("\nIngrese opcion: ");
-
-    scanf("%d",&opcion);
-   }
-    else if(flag1 == 1 && flag2 == 0)
-   {
-    printf("\n *------Menu de opciones------* \n\n");
-    printf("1- Ingresar 1er operando (A = %d).\n",num1);
-    printf("2- Ingresar 2do operando (B = y).\n");
-    printf("3- Calcular todas las operaciones.\n");
-    printf("4- Informar resultados.\n");
-    printf("5- Salir.\n");
-    printf("\n\n=============================\n\n");
-    printf("\nIngrese opcion: ");
-
-    scanf("%d",&opcion);
-   }
-   else if(flag1 == 0 && flag2 == 1)
-   {
-    printf("\n *------Menu de opciones------* \n\n");
-    printf("1- Ingresar 1er operando (A = x).\n");
-    printf("2- Ingresar 2do operando (B = %d).\n", num2);
-    printf("3- Calcular todas las operaciones.\n");
-    printf("4- Informar resultados.\n");
-    printf("5- Salir.\n");
-    printf("\n\n=============================\n\n");
-    printf("\nIngrese opcion: ");
-
-    scanf("%d",&opcion);
-   }
-
-   else
-   {
-    printf("\n *------Menu de opciones------* \n\n");
-    printf("1- Ingresar 1er operando (A = %d).\n",num1);
-    printf("2- Ingresar 2do operando (B = %d).\n",num2);
-    printf("3- Calcular todas las operaciones.\n");
-    printf("4- Informar resultados.\n");
-    printf("5- Salir.\n");
-    printf("\n\n=============================\n\n");
-    printf("\nIngrese opcion: ");
-
-    scanf("%d",&opcion);
-   }
-
-        switch(opcion)
-        {
-            case 1:
-                printf("\n*Ingresar primer numero: ");
-                scanf("%d",&num1);
-                flag1 = 1;
-                system("pause");
-            break;
-            case 2:
-                printf("\n*Ingresar segundo numero: ");
-                scanf("%d",&num2);
-                flag2 = 1;
-                system("pause");
-            break;
-            case 3:
-                if(flag1 == 0 || flag2 == 0)
-                {
-                    printf("\nNo es posible calcular. No se ha ingresado uno o ambos numeros.\n");
-                }
-                else
-                {
-                    printf("\na- Calcular la suma de (%d + %d)\n",num1,num2);
-                    printf("b- Calcular la resta de (%d - %d)\n",num1,num2);
-                    printf("c- Calcular la division de (%d / %d)\n",num1,num2);
-                    printf("d- Calcular la  multiplicacion de (%d * %d)\n",num1,num2);
-                    printf("e- Calcular el factorial de (!%d) y (!%d)\n",num1,num2);
-                    flag3 = 1;
-                    system("pause");
-                }
-            break;
-            case 4:
-                    if(flag1 == 0 || flag2 == 0)
-                {
-                    printf("\nNo es posible mostrar los resultados. Falta ingresar uno o ambos numeros.\n");
-                }
-                    else if(flag3 == 0)
-                {
-                        printf("\nNo es posible mostrar los resultados. Falta realizar las operaciones (opcion 3).");
-                }
-
-                else
-                {
-                    printf("\na- El resultado de %d + %d es: %d\n",num1, num2, sumarDosNumeros(num1, num2));
-                    printf("b- El resultado de %d - %d es: %d\n",num1, num2, restarDosNumeros(num1, num2));
-                    printf("c- El resultado de %d / %d es: %f\n",num1, num2, dividirDosNumeros(num1, num2));
-                    printf("d- El resultado de %d * %d es: %d\n",num1, num2, multiplicarDosNumeros(num1, num2));
-                    printf("e- El factorial de !%d es: %d y el factorial de !%d es: %d\n",num1, factorial(num1), num2, factorial(num2));
-                    system("pause");
-                }
-
-            break;
-            case 5:
-                    printf("\n*Desea salir? s / n: ");
-                    scanf(" %c",&salir);
-            break;
-
-        }
-
-        i++;
-
-    } while ( salir == 'n');
-
-
-
-    return 0;
-}
-*/
